ec9b85f2761716c71ce3a56c95a42d64acb41ae7
Pilo11 <Pilo11@users.noreply.github.com>
2021-01-22 16:09:50 +0100

    Added Chess Client and restructured project

---
+++ ChessSolution/ChessClient/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ChessClient
+{
+    class Program
+    {
+        private static readonly HttpClient _client = new HttpClient();
+        private static bool _isCurrentlyWorking;
+        private static string _player = "w";
+
+        static void Main(string[] args)
+        {
+            Console.Write("White (w) (default) or black (b): ");
+            _player = Console.ReadLine().ToLower() == "b" ? "b" : "w";
+            var timer = new Timer();
+            timer.Interval = 16;
+            timer.Elapsed += new ElapsedEventHandler(Test);
+            timer.Start();
+            Console.Read();
+        }
+
+        private static async void Test(object sender, ElapsedEventArgs e)
+        {
+            if (!_isCurrentlyWorking)
+            {
+                if (WindowsApi.IsUKeyClicked())
+                {
+                    _isCurrentlyWorking = true;
+                    try
+                    {
+                        await DoAnalyze();                        
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    finally
+                    {
+                        _isCurrentlyWorking = false;
+                    }
+                }
+            }
+        }
+
+        private static async Task DoAnalyze()
+        {
+            var handle = GetChessWindowHandle();
+            using Stream imageStream = new MemoryStream();
+            using Bitmap image = WindowsApi.CaptureWindow(handle);
+            using Bitmap imageCopy = image.Clone(new Rectangle(500, 140, 1180, 1180), image.PixelFormat);
+            imageCopy.Save(imageStream, ImageFormat.Png);
+            imageStream.Seek(0, SeekOrigin.Begin);
+            var content = new StreamContent(imageStream);
+            _client.DefaultRequestHeaders.Remove("player");
+            _client.DefaultRequestHeaders.Add("player", _player);
+            var response = await _client.PostAsync("http://10.35.17.1:5000/chess/result", content);
+            var responseString = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(responseString);
+        }        
+
+        private static IntPtr GetChessWindowHandle()
+        {
+            Process[] processes = Process.GetProcessesByName("chrome");
+            foreach (Process p in processes.Where(p => !string.IsNullOrWhiteSpace(p.MainWindowTitle)))
+            {
+                var title = p.MainWindowTitle;
+                if (title.ToLower().Contains("chess.com"))
+                {
+                    return p.MainWindowHandle;
+                }
+            }
+            throw new Exception("Could not find Chess.com window");
+        }
+
+    }
+}
---
+++ ChessSolution/ChessClient/WindowsApi.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ChessClient
+{
+    public class WindowsApi
+    {
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        public static extern IntPtr GetDesktopWindow();
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct Rect
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetWindowRect(IntPtr hWnd, ref Rect rect);
+
+        [DllImport("User32.dll")]
+        static extern short GetAsyncKeyState(int vKey);
+
+        const int U_KEY = 0x55; //This is the U key.
+
+        public static bool IsUKeyClicked()
+        {
+            var keyState = GetAsyncKeyState(U_KEY);
+            return ((keyState >> 15) & 0x0001) == 0x0001;
+        }
+
+        public static Image CaptureDesktop()
+        {
+            return CaptureWindow(GetDesktopWindow());
+        }
+
+        public static Bitmap CaptureActiveWindow()
+        {
+            return CaptureWindow(GetForegroundWindow());
+        }
+
+        public static Bitmap CaptureWindow(IntPtr handle)
+        {
+            var rect = new Rect();
+            GetWindowRect(handle, ref rect);
+            var bounds = new Rectangle(rect.Left, rect.Top, rect.Right - rect.Left, rect.Bottom - rect.Top);
+            var result = new Bitmap(bounds.Width, bounds.Height);
+
+            using (var graphics = Graphics.FromImage(result))
+            {
+                graphics.CopyFromScreen(new Point(bounds.Left, bounds.Top), Point.Empty, bounds.Size);
+            }
+
+            return result;
+        }
+    }
+}
